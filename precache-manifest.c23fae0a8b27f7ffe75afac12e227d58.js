31942bc09e04944ff5af99fdf161c6d98f395c13
Ben <benedry@gmail.com>
2019-07-07 20:08:24 +0300

    Updates

---
+++ precache-manifest.c23fae0a8b27f7ffe75afac12e227d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603aa8d3a69f90cd0ca701de3db6c214",
+    "url": "/okat-cupid/index.html"
+  },
+  {
+    "revision": "6000f5cfbb15df55dbfb",
+    "url": "/okat-cupid/static/css/main.74e0a46a.chunk.css"
+  },
+  {
+    "revision": "9c51689c3592bf68c6db",
+    "url": "/okat-cupid/static/js/2.e4745153.chunk.js"
+  },
+  {
+    "revision": "6000f5cfbb15df55dbfb",
+    "url": "/okat-cupid/static/js/main.e0e0738e.chunk.js"
+  },
+  {
+    "revision": "ac4d2b7ca81d0f804757",
+    "url": "/okat-cupid/static/js/runtime~main.2982c164.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/okat-cupid/precache-manifest.132bbc19f53c7cc9e3f4d7fc12b7d291.js"
+  "/okat-cupid/precache-manifest.c23fae0a8b27f7ffe75afac12e227d58.js"
 );
 
 self.addEventListener('message', (event) => {
